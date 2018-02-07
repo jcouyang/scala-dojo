26cf28dcd30fd134d4996d34bc6bf2b26abe94fd
Jichao Ouyang <oyanglulu@gmail.com>
2018-02-07 23:46:18 +1100

    hide all the answers

--- src/main/scala/ObjectAndClass.scala
+++ src/main/scala/ObjectAndClass.scala
@@ -3,18 +3,11 @@ package classandobject
 // * Companion object
 class Person(val firstName: String, val lastName: String) {}
 
-object Person {
-  def apply(fullName: String) = {
-    val parts = fullName.split(" ")
-    new Person(parts(0), parts(1))
-  }
-}
+object Person {}
 
 // * Pattern Matching
 case class Cat(color: String, food: String)
 
-object ChipShop {
-  def willServe(cat: Cat) = true
-}
+object ChipShop {}
 
 
--- src/main/scala/Typeclass.scala
+++ src/main/scala/Typeclass.scala
@@ -1,13 +1,7 @@
 package typeclass
 
 sealed trait TrafficLight {
-  def next = {
-    this match {
-      case Red => Green
-      case Green => Yellow
-      case Yellow => Red
-    }
-  }
+  def next = ???
 }
 
 final case object Red extends TrafficLight
@@ -15,31 +9,14 @@ final case object Green extends TrafficLight
 final case object Yellow extends TrafficLight
 
 sealed trait LinkedList[A] {
-  def apply(index: Int): A =
-    this match {
-      case Pair(head, tail) =>
-        if(index == 0)
-          head
-        else
-          tail(index - 1)
-      case End() =>
-        throw new Exception("Out of Boundary")
-    }
+  def apply(index: Int): A = ???
 }
+
 final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
 final case class End[A]() extends LinkedList[A]
 
-sealed trait CoLinkedList[+A] {
-  def apply(index: Int): A =
-    this match {
-      case CoPair(head, tail) =>
-        if(index == 0)
-          head
-        else
-          tail(index - 1)
-      case CoEnd =>
-        throw new Exception("Out of Boundary")
-    }
+sealed trait CoLinkedList[A] {
+  def apply(index: Int): A = ???
 }
 final case class CoPair[A](head: A, tail: CoLinkedList[A]) extends CoLinkedList[A]
 final case object CoEnd extends CoLinkedList[Nothing]
@@ -49,40 +26,31 @@ trait JsonWriter[A] {
 }
 
 object JsonWriter {
-  def write[A](a:A)(implicit writer:JsonWriter[A]) = writer.write(a)
-  implicit class Ops[A: JsonWriter](a: A) {
-    def writeJson = JsonWriter.write(a)
+  def write[A](a:A) = ???
+  implicit class Ops[A](a: A) {
+    def writeJson = ???
   }
 }
 
 final case class Person(name: String, email: String)
 
 object Person {
-  implicit val jsonWriterForPerson = new JsonWriter[Person] {
-    def write(p: Person) =
-      s"""{"name": "${p.name}", "email": "${p.email}"}"""
-  }
-  implicit val sortablePerson = Ordering.fromLessThan[Person]((x, y) =>
-    x.name < y.name
-  )
+  implicit val jsonWriterForPerson = ???
+  // hint: https://www.scala-lang.org/api/current/scala/math/Ordering$.html#fromLessThan[T](cmp:(T,T)=%3EBoolean):scala.math.Ordering[T]
+  implicit val sortablePerson = ???
 }
 
 final case class Cat(name: String, food: String)
 
 object Cat {
-  implicit val jsonWriterForCat = new JsonWriter[Cat] {
-    def write(p: Cat) =
-      s"""{"name": "${p.name}", "food": "${p.food}"}"""
-  }
+  implicit val jsonWriterForCat = ???
 }
 
 final case class CatPerson(person: Person, cat: Cat)
 
 object CatPerson {
-  implicit val jsonWriterForCatPerson = new JsonWriter[CatPerson] {
-    def write(cp: CatPerson) = s"""{"person":${JsonWriter.write(cp.person)},"cat":"${JsonWriter.write(cp.cat)}"}"""
-  }
+  implicit val jsonWriterForCatPerson = ???
   implicit class CatPersonOps(cp: CatPerson) {
-    def writeJson = JsonWriter.write(cp)
+    def writeJson = ???
   }
 }
