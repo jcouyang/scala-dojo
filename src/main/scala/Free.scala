a949b39866690515556cc1807af51e742e827ddf
Jichao Ouyang <oyanglulu@gmail.com>
2018-11-13 13:52:40 +1100

    rank 2 type

--- src/main/scala/Free.scala
+++ src/main/scala/Free.scala
@@ -12,4 +12,15 @@ object Sphere {
   implicit def spherePrintable[A](implicit p: Printable[Box[A]],
                                   fnk: (Sphere ~> Box)): Printable[Sphere[A]] =
     Contravariant[Printable].contramap(p)(fnk.apply)
+
+  implicit def tuplePrintable[A, B](
+      implicit p: Printable[Box[String]],
+      fnk: (Sphere ~> Box)): Printable[(Sphere[A], Sphere[B])] = {
+    val tupleOfSphereToBox = (tupleOfSphere: (Sphere[A], Sphere[B])) => {
+      val box1 = fnk(tupleOfSphere._1)
+      val box2 = fnk(tupleOfSphere._2)
+      Box(s"(${box1.value}, ${box2.value})")
+    }
+    Contravariant[Printable].contramap(p)(tupleOfSphereToBox)
+  }
 }
--- src/test/scala/4-1-kind.scala
+++ src/test/scala/4-1-kind.scala
@@ -4,9 +4,14 @@ import org.scalatest._
 
 class `4-1-Kind` extends AsyncFlatSpec with Matchers {
 
-behavior of "FunctionK"
-it should "create a FunctionK from `Box[_]` to `Sphere[_]`" in {
-  Printable.format(Sphere("hill")) shouldBe "\"hill\""
-}
+  behavior of "FunctionK"
+  it should "create a FunctionK from Box[_] to Sphere[_]" in {
+    Printable.format(Sphere("hill")) shouldBe "\"hill\""
+  }
+
+  behavior of "Rank N Type"
+  it should "able to print a tuple of Sphere" in {
+    Printable.format((Sphere("hill"), Sphere(1))) shouldBe "\"(hill, 1)\""
+  }
 
 }
